9f204723e534c46a8ca7834c97c600d28c6863c5
Darryl Melander <darryl.melander@sv.cmu.edu>
2011-02-05 21:03:55 +0100

    syntax fixes for gravity functor

--- src/vw/orbital_refinement/GravityAccelerationFunctor.hpp
+++ src/vw/orbital_refinement/GravityAccelerationFunctor.hpp
@@ -1,21 +1,29 @@
 #ifndef GRAVITY_ACCELERATION_FUNCTOR_HPP
 #define GRAVITY_ACCELERATION_FUNCTOR_HPP
 
-#include <vw/Match/Vector.h>
+#include <vw/Math/Vector.h>
+
+namespace GravityConstants
+{
+    //! Gravitational Constant for the moon's mass,
+    //! in m^3/s^2
+    const double GM_MOON = 6.67259e-11*7.36e22;
+}
+
 
 //! A function object which calculates the acceleration experienced
 //! by an entity at some position relative to a gravitational body.
 class GravityAccelerationFunctor
 {
 public:
-  //! Gravitational Constant for the moon's mass,
-  //! in m^3/s^2
-  const double GM_MOON = 6.67259e-11*7.36e22;
 
     //! Constructor.
     //! \param GM The gravitational constant to use.
     //! Defaults to GM_MOON
-  GravityAccelerationFunctor(double GM=GM_MOON);
+  GravityAccelerationFunctor(double GM=GravityConstants::GM_MOON)
+          : _GM(GM)
+      {}
+  
 
     //! Change the gravitational constant.
   void setGM(double GM)
@@ -25,11 +33,11 @@ public:
     //! \param x The position of the entity relative to the center
     //!        of the gravitational body.  Units should be consistent
     //!        with the functor's current GM value.
-  Vector3 operator()(const Vector3& x)
+  vw::Vector3 operator()(const vw::Vector3& x) const
       {
-        double r_squared = x.length_squared();
+        double r_squared = dot_prod(x, x);
         double r = sqrt(r_squared);
-        return x*(-GM/(r*r_squared));
+        return x*(-_GM/(r*r_squared));
       }
 
 private:
