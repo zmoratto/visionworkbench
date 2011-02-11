2b9ae77c8f92a10d6ebf8a1bfbe0f5ccb64c258c
Darryl Melander <darryl.melander@sv.cmu.edu>
2011-02-11 05:45:22 +0100

    Set up testing infrastructure for orbital_refinement

---
+++ src/vw/orbital_refinement/tests/TestTrajectoryCalculator.cpp
@@ -0,0 +1,15 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2010 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+
+// TestConjugateGradient.h
+#include <gtest/gtest.h>
+#include <vw/orbital_refinement/TrajectoryCalculator.hpp>
+
+TEST( TrajectorCalculator, SanityCheck )
+{
+  EXPECT_EQ(1, 1);
+}
