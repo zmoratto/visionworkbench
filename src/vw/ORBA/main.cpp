d94d8df5d34f51a8193a05892996f3a519ebc0bf
hfung <henry.fung@sv.cmu.edu>
2011-04-12 01:23:14 -0700

    updated main.cpp for reading in timestamp file and setting up camera names

--- src/vw/ORBA/main.cpp
+++ src/vw/ORBA/main.cpp
@@ -12,6 +12,7 @@
 
 #include <boost/foreach.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
 #include <cstdlib>
 #include <fstream>
@@ -24,18 +25,66 @@ using namespace vw::camera;
 using namespace vw::ORBA;
 using namespace vw::ba;
 
-void write_results (std::string file, std::list<OrbitalCameraReading> data)
+void write_results (std::string file, std::vector<OrbitalCameraReading> data)
 {
     // Do something nice to convert to CSV
   ofstream output( file.c_str() );
-  for (std::list<OrbitalCameraReading>::iterator it = data.begin();
+  for (std::vector<OrbitalCameraReading>::iterator it = data.begin();
        it != data.end();
        it++)
   {
-    output << it->mCoord << it->mCamera->camera_pose() << "\n";
+    output << it->mId << "," 
+           << it->mCoord[0] << ","
+           << it->mCoord[1] << ","
+           << it->mCoord[2] << ","
+           << it->mCamera->camera_pose() << ","
+           << it->mTime << "\n";
   }
   
   output.close();
+}
+
+void read_input (std::string filename, std::vector<std::string>& camera_names,
+                 std::vector<OrbitalReading::timestamp_t>& times)
+{
+  std::string entry_id_string, time_string;
+
+  // Open the file
+  std::ifstream input_file(filename.c_str());
+  if (!input_file)
+    return false;
+
+  // Read each line.
+  // Note that this code only parses the format found in our sample data,
+  // where none of the fields have any of the "interesting" features of
+  // CSV like embedded commas.
+  std::string line_buffer;
+  std::istringstream line;
+  while (std::getline(input_file, line_buffer))
+  {
+    // Get the string ready to parse
+    line.clear();
+    line.str(line_buffer);
+
+    // The dummy char 'c' reads the comma characters.
+    char c;
+
+    // Read the next line of data
+    std::getline(line, entry_id_string, ',');
+    std::getline(line, time_string, ',');
+
+    if (line.fail())
+      break;
+
+    camera_names.push_back(entry_id_string);
+
+    // Not sure of the logic here?
+    double time = atof(time_string);
+    time_t ts = static_cast<time_t>(time);
+    OrbitalReading::timestamp_t result = ts;
+
+    times.push_back(result);
+  }
 }
 
 /*
@@ -44,18 +93,39 @@ void write_results (std::string file, std::list<OrbitalCameraReading> data)
 int main(int argc, char** argv)
 {
   std::vector<std::string> camera_names;
-  std::string file_name;
+  std::vector<OrbitalReading::timestamp_t> times;
+  std::string file_name = "a15_rev033_ephemeris.time";
 
-  // Check that sigmas have been passed in
-  if (argc < 4)
-  {
-      cout << "USAGE: orba_utility <projection variance> <registration variance> <satellite variance> <timing variance>" << endl;
-      return 1;
+  // Setup variables to store variances
+  double var_p;
+  double var_r;
+  double var_s;
+  double var_t;
+
+  // Setup the input options
+  namespace po = boost::program_options;
+  po::options_description desc("Allowed options:");
+   desc.add_options()
+    ("help", "Produce help message")
+    ("projection", po::value<double>(&var_p)->default_value(0), "Set projection variance")
+    ("registration", po::value<double>(&var_r)->default_value(0), "Set registration variance")
+    ("satellite", po::value<double>(&var_s)->default_value(0), "Set satellite variance")
+    ("timing", po::value<double>(&var_t)->default_value(0), "Set timing variance")
+   ;
+
+  // Setup variable map
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  // Display help message if option was called
+  if (vm.count("help")) {
+    cout << desc << "\n";
+    return 1;
   }
 
-  
-  double sigma_t = atof(argv[3]);
 
+/*
 // Load the pinhole models
   boost::filesystem::path my_path("/a15_rev033/");
     // default construction yields past-the-end
@@ -67,23 +137,27 @@ int main(int argc, char** argv)
     file_name = itr->leaf().substr(itr->leaf().find_last_of(".") + 1);
     if(file_name == "pinhole")
       camera_names.push_back(itr->leaf());
-  }
+  }*/
+
+  read_input(file_name, camera_names, times);
 
   ObservationSet observations;
   ObservationSet dest;
   observations.setExpectedReadingCount(camera_names.size());
 
-  std::vector<boost::shared_ptr<PinholeModel> > cameras;
+  // Counter for timestamps, should be same order as camera names
+  int i = 0;
+
   BOOST_FOREACH( std::string const& in, camera_names )
   {
-      // Where does this really come from?
-    OrbitalReading::timestamp_t timestamp = 0;
     
     boost::shared_ptr<PinholeModel> new_cam(new PinholeModel(in));
     
-      // Create a reading from the camera, store it in the observations
-    observations.addReading(OrbitalCameraReading("Where does the ID come from?",
-                                                 timestamp, new_cam));
+    // Create a reading from the camera, store it in the observations
+    observations.addReading(OrbitalCameraReading(in, times[i], new_cam));
+
+    // Increment the counter for the timestamp vector
+    i++;
   }
 
   // Load the control network
@@ -94,36 +168,17 @@ int main(int argc, char** argv)
   // Now set the control network in the observation set
   observations.setControlNetwork(network);
 
-  // Sort the observations by time before passing into the refiner
-  observations.getReadings().sort(OrbitalReading::TimestampLess());
+  // Setup the variances to pass into refiner
+  const Vector2 sigma_p(var_p);
+  const Vector3 sigma_r(var_r);
+  const Vector3 sigma_s(var_s);
+  const double sigma_t = var_t;
 
-
-  // ***
-  // are the sigmas being passed in by command line? what format? some of the
-  // sigmas are represented as vectors...
-  // ***
   ORBARefiner refiner;
-  refiner.refineORBAReadings(observations, dest);
+  refiner.refineORBAReadings(observations, dest, sigma_p, sigma_r, sigma_s, sigma_t);
 
-#if 0
-    // This logic should be encapsulated in the ORBARefiner, not here in the
-    // main function
-  
-
-    // Perform first pass with that data that is completely OR
-  OrbitalRefiner first_pass;
-    //first_pass.refine( data, intermediate );
-
- // Perform second pass with OR-BA
- //OrbitalBARefiner second_pass;
- /*second_pass.refine( cnet, intermediate, data,
-                     user_input_variance_s, user_input_variance_t,
-                     user_input_variance_p, user_input_variance_r );
-
- // Write out data into CSV
- write_results( file, data );*/
-
-#endif
+  // Write to a file
+  write_results("a15_rev033_refined.csv", dest.getReadings());
 
   return 0;
 }
